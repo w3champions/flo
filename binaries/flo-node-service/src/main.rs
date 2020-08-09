c6c0d385da1b9c525524913f3bded0ccfebf4f47
Flux Xu <fluxxu@gmail.com>
2020-08-09 12:15:34 -0400

    refactor: do not touch env in production

--- binaries/flo-node-service/src/main.rs
+++ binaries/flo-node-service/src/main.rs
@@ -2,9 +2,11 @@ use flo_node::{serve_echo, serve_node};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-  dotenv::dotenv()?;
-
-  flo_log::init_env("flo_node_service=debug,flo_node=debug");
+  #[cfg(debug_assertions)]
+  {
+    dotenv::dotenv()?;
+    flo_log::init_env("flo_node_service=debug,flo_node=debug");
+  }
 
   tokio::try_join!(serve_echo(), serve_node())?;
 
