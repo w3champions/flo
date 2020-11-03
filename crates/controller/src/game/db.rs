7f8360c9c6b4722b8e345260071b9355d1c5be14
Flux Xu <fluxxu@gmail.com>
2020-11-03 17:51:06 -0500

    fix: fix slot with Left status still active

--- crates/controller/src/game/db.rs
+++ crates/controller/src/game/db.rs
@@ -520,6 +520,7 @@ pub fn get_player_active_slots(conn: &DbConn, player_id: i32) -> Result<Vec<Play
     .select((game_used_slot::game_id, UsedSlotInfo::columns()))
     .filter(game::status.eq(any(GameStatus::active_variants())))
     .filter(game_used_slot::player_id.eq(player_id))
+    .filter(game_used_slot::client_status.ne(SlotClientStatus::Left))
     .order(game_used_slot::created_at)
     .load(conn)?;
   Ok(rows)
