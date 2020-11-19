84966f27a089b04844b7acd15e1d24de2256fd9d
Flux Xu <fluxxu@gmail.com>
2020-11-19 00:48:40 -0500

    feat: add flo-worker

---
+++ binaries/flo-worker/src/log.rs
@@ -0,0 +1,42 @@
+use tracing_subscriber::filter::LevelFilter;
+use tracing_subscriber::EnvFilter;
+
+#[cfg(debug_assertions)]
+pub fn init(debug: bool) {
+  let filter = EnvFilter::from_default_env()
+    // Set the base level when not matched by other directives to WARN.
+    .add_directive(if debug {
+      LevelFilter::DEBUG.into()
+    } else {
+      LevelFilter::WARN.into()
+    });
+
+
+  tracing_subscriber::fmt().with_env_filter(filter).init();
+}
+
+#[cfg(not(debug_assertions))]
+pub fn init(debug: bool) {
+  use once_cell::sync::OnceCell;
+  use tracing_appender::non_blocking::WorkerGuard;
+  static INSTANCE: OnceCell<WorkerGuard> = OnceCell::new();
+
+  let file_appender = tracing_appender::rolling::daily("flo-logs", "flo.log");
+  let (non_blocking, guard) = tracing_appender::non_blocking(file_appender);
+
+  INSTANCE.set(guard).unwrap();
+
+  let filter = EnvFilter::from_default_env()
+    // Set the base level when not matched by other directives to WARN.
+    .add_directive(if debug {
+      LevelFilter::DEBUG.into()
+    } else {
+      LevelFilter::WARN.into()
+    });
+
+  tracing_subscriber::fmt()
+    .with_env_filter(filter)
+    .with_writer(non_blocking)
+    .with_ansi(false)
+    .init();
+}
---
+++ binaries/flo-worker/src/main.rs
@@ -0,0 +1,67 @@
+mod log;
+
+use anyhow::Result;
+use flo_client::StartConfig;
+use std::io::Write;
+use std::path::PathBuf;
+use structopt::StructOpt;
+use tokio::runtime::Runtime;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "flo-worker", about = "Flo worker process.")]
+struct Opt {
+  #[structopt(long)]
+  debug: bool,
+
+  #[structopt(long)]
+  token: Option<String>,
+
+  #[structopt(long, parse(from_os_str))]
+  installation_path: Option<PathBuf>,
+}
+
+fn main() {
+  #[cfg(windows)]
+  unsafe {
+    winapi::um::processthreadsapi::SetPriorityClass(
+      winapi::um::processthreadsapi::GetCurrentProcess(),
+      winapi::um::winbase::HIGH_PRIORITY_CLASS,
+    );
+  }
+
+  let opt = Opt::from_args();
+
+  log::init(opt.debug);
+
+  let res = std::panic::catch_unwind(|| -> Result<_> {
+    let mut rt = Runtime::new()?;
+    let client = rt.block_on(flo_client::start(StartConfig {
+      token: opt.token,
+      installation_path: opt.installation_path,
+    }))?;
+    let port = client.port();
+    rt.spawn(client.serve());
+    Ok((port, rt))
+  })
+  .map_err(|err| anyhow::format_err!("Start flo worker failed: {:?}", err))
+  .and_then(std::convert::identity);
+
+  match res {
+    Ok((port, mut rt)) => {
+      let msg = serde_json::to_string(&serde_json::json!({
+        "version": flo_client::FLO_VERSION.to_string(),
+        "port": port
+      })).unwrap();
+      let mut stdout = std::io::stdout();
+      stdout.write(msg.as_bytes()).unwrap();
+      stdout.flush().unwrap();
+      rt.block_on(tokio::signal::ctrl_c()).unwrap();
+    }
+    Err(err) => {
+      let msg = serde_json::to_string(&serde_json::json!({
+        "error": err.to_string()
+      })).unwrap();
+      std::io::stderr().write(msg.as_bytes()).unwrap();
+    }
+  }
+}
