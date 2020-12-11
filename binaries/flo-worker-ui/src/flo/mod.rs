94e310f734987f6e7c91c2f5ab9f395cc6ee96a8
Qeenon <codemasha@gmail.com>
2020-12-11 23:58:23 +0400

    fix running flo worker but dropped catch_unwind

--- binaries/flo-worker-ui/src/flo/mod.rs
+++ binaries/flo-worker-ui/src/flo/mod.rs
@@ -3,28 +3,35 @@ use super::Opt;
 use anyhow::Result;
 use flo_client::StartConfig;
 use tokio::runtime::Runtime;
+use tokio::sync::Mutex;
 
-pub fn run_flo(opt: Opt) -> (bool, String) {
-  let res = std::panic::catch_unwind(|| -> Result<_> {
-    let mut rt = Runtime::new()?;
-    let client = rt.block_on(flo_client::start(StartConfig {
-      token: opt.token,
-      installation_path: opt.installation_path,
-      controller_host: opt.controller_host.clone(),
-      ..Default::default()
-    }))?;
-    let port = client.port();
-    rt.spawn(client.serve());
-    Ok((port, rt))
-  })
-  .map_err(|err| anyhow::format_err!("Start flo worker failed: {:?}", err))
-  .and_then(std::convert::identity);
+use once_cell::sync::OnceCell;
+use once_cell::sync::Lazy;
+
+static RT: Lazy<Mutex<Runtime>> = Lazy::new(|| { Mutex::new(Runtime::new().unwrap()) });
+
+async fn run_flo(opt: Opt) -> Result<u16> {
+  let mut rt = RT.lock().await;
+  let client = rt.block_on(flo_client::start(StartConfig {
+    token: opt.token,
+    installation_path: opt.installation_path,
+    controller_host: opt.controller_host.clone(),
+    ..Default::default()
+  }))?;
+  let port = client.port();
+  rt.spawn(
+    client.serve()
+  );
+  Ok(port)
+}
+
+pub async fn perform_run_flo(opt: Opt) -> (bool, String) {
+  let mut res = run_flo(opt).await
+    .map_err(|err| anyhow::format_err!("Start flo worker failed: {:?}", err));
 
   match res {
-    Ok((port, mut _rt)) => {
+    Ok(port) => {
       tracing::info!("running on port: {}", port);
-      // it's inside GUI, no need to block on it I guess
-      // rt.block_on(tokio::signal::ctrl_c()).unwrap();
       (true, port.to_string())
     }
     Err(err) => {
@@ -32,9 +39,4 @@ pub fn run_flo(opt: Opt) -> (bool, String) {
       (false, err.to_string())
     }
   }
-}
-
-#[allow(dead_code)]
-pub async fn perform_run_flo(opt: Opt) -> (bool, String) {
-  run_flo(opt)
 }
