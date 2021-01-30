12e1cb55cc9df165308f847334fe21e1352d23f9
Qeenon <codemasha@gmail.com>
2021-01-30 19:28:42 +0400

    ingame commands: for -stats allow name argument if it's not a number

--- crates/client/src/lan/game/game.rs
+++ crates/client/src/lan/game/game.rs
@@ -314,8 +314,8 @@ impl<'a> GameHandler<'a> {
               self.info.slot_info.slot_player_id, targets, solo);
           }
         } else {
-          let id = &cmd["stats ".len()..];
-          if let Some(id) = id.parse::<u8>().ok() {
+          let id_or_name = &cmd["stats ".len()..];
+          if let Ok(id) = id_or_name.parse::<u8>() {
             let targets: Vec<(String, u32)> = players
               .iter()
               .filter_map(|slot|
@@ -338,11 +338,27 @@ impl<'a> GameHandler<'a> {
               self.send_chats_to_self(self.info.slot_info.slot_player_id, msgs);
             }
           } else {
-            let mut msgs = vec![format!("Type `!stats <ID>` to get stats for:")];
-            for slot in &self.info.slot_info.player_infos {
-              msgs.push(format!(" ID={} {}", slot.slot_player_id, slot.name.as_str()));
+            let targets: Vec<(String, u32)> = players
+              .iter()
+              .filter_map(|slot|
+                if slot.name.to_lowercase().starts_with(&id_or_name.to_lowercase()) {
+                  Some(( slot.name.clone()
+                       , self.info.game.slots[slot.slot_index].settings.race as u32 ))
+                } else {
+                  None
+                }
+              )
+              .collect();
+            if !targets.is_empty() {
+              self.send_stats_to_self(
+                self.info.slot_info.slot_player_id, targets, solo);
+            } else {
+              let mut msgs = vec![format!("Type `!stats <ID>` to get stats for:")];
+              for slot in &self.info.slot_info.player_infos {
+                msgs.push(format!(" ID={} {}", slot.slot_player_id, slot.name.as_str()));
+              }
+              self.send_chats_to_self(self.info.slot_info.slot_player_id, msgs);
             }
-            self.send_chats_to_self(self.info.slot_info.slot_player_id, msgs);
           }
         }
       }
@@ -401,7 +417,7 @@ impl<'a> GameHandler<'a> {
           } else {
             &cmd["mute ".len()..]
           };
-          if let Some(id) = id.parse::<u8>().ok() {
+          if let Ok(id) = id.parse::<u8>() {
             if let Some(info) = self
               .info
               .slot_info
