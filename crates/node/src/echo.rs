468c0e63091bcf6ac77e97076b96330087c4ac90
Flux Xu <fluxxu@gmail.com>
2021-02-27 21:52:16 -0500

    fix: support both 4 and 8 bytes echo

--- crates/node/src/echo.rs
+++ crates/node/src/echo.rs
@@ -1,7 +1,8 @@
 use crate::error::Result;
 use std::net::{Ipv4Addr, SocketAddrV4};
 use tokio::net::UdpSocket;
-const ECHO_DATAGRAM_LEN: usize = 8;
+const ALLOWED_ECHO_DATAGRAM_LEN: &[usize] = &[4, 8];
+const MAX_RECV_BUF: usize = 8;
 
 use flo_constants::NODE_ECHO_PORT;
 
@@ -9,14 +10,14 @@ pub async fn serve_echo() -> Result<()> {
   let mut socket =
     UdpSocket::bind(SocketAddrV4::new(Ipv4Addr::UNSPECIFIED, NODE_ECHO_PORT)).await?;
 
-  let mut recv_buf = [0_u8; ECHO_DATAGRAM_LEN];
+  let mut recv_buf = [0_u8; MAX_RECV_BUF];
 
   loop {
     if let Some((size, peer)) = socket.recv_from(&mut recv_buf).await.ok() {
-      if size != ECHO_DATAGRAM_LEN {
+      if ALLOWED_ECHO_DATAGRAM_LEN.contains(&size) {
         continue;
       }
-      socket.send_to(&recv_buf[..], &peer).await.ok();
+      socket.send_to(&recv_buf[..size], &peer).await.ok();
     }
   }
 }
