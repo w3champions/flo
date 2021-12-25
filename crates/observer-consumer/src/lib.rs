4b7d1fde90421244ac7277a436d69ccbf75ae1e3
Flux Xu <fluxxu@gmail.com>
2021-12-25 17:03:58 -0500

    make Archiver optional

--- crates/observer-consumer/src/lib.rs
+++ crates/observer-consumer/src/lib.rs
@@ -34,7 +34,7 @@ impl FloObserver {
 
 pub(crate) struct ShardsMgr {
   cache: Persist,
-  uploader_handle: ArchiverHandle,
+  uploader_handle: Option<ArchiverHandle>,
   shard_ids: Vec<String>,
   shards: BTreeMap<String, Owner<ShardConsumer>>,
 }
@@ -44,8 +44,14 @@ impl ShardsMgr {
     use rusoto_kinesis::ListShardsInput;
 
     let cache = Persist::connect().await?;
-    let (uploader, uploader_handle) = Archiver::new(GameDataWriter::data_folder().to_owned())?;
-    tokio::spawn(uploader.serve());
+    let uploader_handle = if let Some((uploader, uploader_handle)) =
+      Archiver::new(GameDataWriter::data_folder().to_owned())?
+    {
+      tokio::spawn(uploader.serve());
+      Some(uploader_handle)
+    } else {
+      None
+    };
 
     let shards = KINESIS_CLIENT
       .list_shards(ListShardsInput {
