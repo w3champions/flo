f2773fafe278e3cb1c9bdccfaf4da7c270ce9ff2
Flux Xu <fluxxu@gmail.com>
2022-01-29 19:22:14 -0500

    fix(stats): handle cors

--- binaries/flo-stats-service/src/main.rs
+++ binaries/flo-stats-service/src/main.rs
@@ -1,13 +1,16 @@
 mod graphql;
 
+use crate::graphql::{FloLiveSchema, MutationRoot, QueryRoot, SubscriptionRoot};
 use async_graphql::http::{playground_source, GraphQLPlaygroundConfig};
 use async_graphql::Schema;
 use async_graphql_axum::{GraphQLRequest, GraphQLResponse, GraphQLSubscription};
+use axum::headers::HeaderValue;
+use axum::http::Method;
 use axum::response::{self, IntoResponse};
 use axum::routing::get;
 use axum::{extract, AddExtensionLayer, Router, Server};
 use flo_observer_edge::FloObserverEdge;
-use crate::graphql::{FloLiveSchema, QueryRoot, MutationRoot, SubscriptionRoot};
+use tower_http::cors::{CorsLayer, Origin};
 
 async fn graphql_handler(
   schema: extract::Extension<FloLiveSchema>,
@@ -18,7 +21,7 @@ async fn graphql_handler(
 
 async fn graphql_playground() -> impl IntoResponse {
   response::Html(playground_source(
-      GraphQLPlaygroundConfig::new("/").subscription_endpoint("/ws"),
+    GraphQLPlaygroundConfig::new("/").subscription_endpoint("/ws"),
   ))
 }
 
@@ -27,9 +30,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
   #[cfg(debug_assertions)]
   {
     dotenv::dotenv()?;
-    flo_log_subscriber::init_env_override(
-      "flo_stats_service=debug,flo_observer_edge=debug",
-    );
+    flo_log_subscriber::init_env_override("flo_stats_service=debug,flo_observer_edge=debug");
   }
 
   #[cfg(not(debug_assertions))]
@@ -40,8 +41,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
   let edge = FloObserverEdge::from_env().await?;
 
   let schema = Schema::build(QueryRoot, MutationRoot, SubscriptionRoot)
-      .data(edge.handle())
-      .finish();
+    .data(edge.handle())
+    .finish();
 
   tokio::spawn(async move {
     if let Err(err) = edge.serve().await {
@@ -50,16 +51,26 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
   });
 
   let app = Router::new()
-      .route("/", get(graphql_playground).post(graphql_handler))
-      .route("/ws", GraphQLSubscription::new(schema.clone()))
-      .layer(AddExtensionLayer::new(schema));
+    .route("/", get(graphql_playground).post(graphql_handler))
+    .route("/ws", GraphQLSubscription::new(schema.clone()))
+    .layer(AddExtensionLayer::new(schema))
+    .layer({
+      let allowed_list: [HeaderValue; 2] = [
+        "http://localhost:3000".parse().unwrap(),
+        "https://w3flo.com".parse().unwrap(),
+      ];
+      CorsLayer::new()
+        .allow_origin(Origin::list(allowed_list))
+        .allow_methods(vec![Method::POST])
+        .allow_headers(tower_http::cors::any())
+    });
 
   let bind = format!("0.0.0.0:{}", flo_constants::OBSERVER_GRAPHQL_PORT);
-  
+
   tracing::info!("running at {}", bind);
 
   Server::bind(&bind.parse().unwrap())
-      .serve(app.into_make_service())
-      .await?;
+    .serve(app.into_make_service())
+    .await?;
   Ok(())
 }
--- crates/observer-edge/src/server/send_queue.rs
+++ crates/observer-edge/src/server/send_queue.rs
@@ -1,12 +1,13 @@
+use crate::game::stream::GameStreamFrame;
 use flo_net::packet::{Frame, PacketTypeId};
 use futures::Stream;
 use std::{
+  collections::VecDeque,
   pin::Pin,
   task::{Context, Poll, Waker},
-  time::{Duration, SystemTime, Instant}, collections::VecDeque,
+  time::{Duration, Instant, SystemTime},
 };
 use tokio_util::time::DelayQueue;
-use crate::game::stream::GameStreamFrame;
 
 pub trait GameStreamSendQueue: Stream<Item = Frame> + Unpin + Send + 'static {
   fn push_frames(&mut self, frames: &[GameStreamFrame]);
@@ -34,9 +35,10 @@ impl NoDelaySendQueue {
     }
 
     for frame in frames {
-      self
-        .q
-        .push_back(Frame::new_bytes(PacketTypeId::ObserverData, frame.data.clone()));
+      self.q.push_back(Frame::new_bytes(
+        PacketTypeId::ObserverData,
+        frame.data.clone(),
+      ));
     }
     if !self.q.is_empty() {
       self.w.take().map(|w| w.wake());
@@ -103,8 +105,12 @@ impl DelaySendQueue {
   const MAX_PRESEND_MILLIS: i64 = 20_000;
 
   pub fn new(delay_millis: i64) -> Self {
-    let now = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).ok().unwrap_or_default();
-    let fast_forwarding_millis = (delay_millis as f64 / (flo_constants::OBSERVER_FAST_FORWARDING_SPEED - 1.0)).ceil() as i64;
+    let now = SystemTime::now()
+      .duration_since(SystemTime::UNIX_EPOCH)
+      .ok()
+      .unwrap_or_default();
+    let fast_forwarding_millis =
+      (delay_millis as f64 / (flo_constants::OBSERVER_FAST_FORWARDING_SPEED - 1.0)).ceil() as i64;
 
     tracing::debug!("fast_forwarding_millis = {}", fast_forwarding_millis);
 
@@ -132,25 +138,29 @@ impl DelaySendQueue {
 
     let now = self.base_timestamp_millis + (Instant::now() - self.base_instant).as_millis() as i64;
     for frame in frames {
-      let fast_forwarding = now.saturating_sub(frame.approx_timestamp_millis) <= self.fast_forwarding_millis;
+      let fast_forwarding =
+        now.saturating_sub(frame.approx_timestamp_millis) <= self.fast_forwarding_millis;
       let delay_millis = if fast_forwarding {
-        (self.delay_millis as f64 / flo_constants::OBSERVER_FAST_FORWARDING_SPEED).round() as i64
+        (self.delay_millis as f64 / flo_constants::OBSERVER_FAST_FORWARDING_SPEED).floor() as i64
       } else {
         self.delay_millis
       };
 
-      let delay = (frame.approx_timestamp_millis + delay_millis).saturating_sub(now).saturating_sub(Self::MAX_PRESEND_MILLIS);
+      let delay = (frame.approx_timestamp_millis + delay_millis)
+        .saturating_sub(now)
+        .saturating_sub(Self::MAX_PRESEND_MILLIS);
 
       // tracing::debug!("frame: approx_timestamp_millis = {}, delay = {}", frame.approx_timestamp_millis, delay);
 
-      let delay = Duration::from_millis(std::cmp::max(
-        0_i64, 
-        delay
-      ) as u64);
+      let delay = Duration::from_millis(std::cmp::max(0_i64, delay) as u64);
 
-      self
-          .delayed
-          .insert((self.next_seq, Frame::new_bytes(PacketTypeId::ObserverData, frame.data.clone())),  delay);
+      self.delayed.insert(
+        (
+          self.next_seq,
+          Frame::new_bytes(PacketTypeId::ObserverData, frame.data.clone()),
+        ),
+        delay,
+      );
       self.next_seq = self.next_seq.saturating_add(1);
     }
 
