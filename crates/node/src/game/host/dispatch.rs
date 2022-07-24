790cf0adf8c79d7867861987c875f886b406b637
Arnaud Esteve <arnaud.esteve@gmail.com>
2022-07-24 11:46:17 +0200

    no type annotation needed

--- crates/node/src/game/host/dispatch.rs
+++ crates/node/src/game/host/dispatch.rs
@@ -38,6 +38,9 @@ use tokio::time::{interval_at, sleep, MissedTickBehavior};
 use tokio_util::sync::CancellationToken;
 use tracing_futures::Instrument;
 
+
+use std::iter::Iterator;
+
 const DISPATCH_ACTIONS_MTU: usize = 1350 - 8;
 
 #[derive(Debug)]
@@ -125,7 +128,8 @@ impl Dispatcher {
         .flat_map(|id| {
           state._player_name_lookup.get(&id).cloned()
         })
-        .collect::<Vec<String>>();
+        .collect();
+
     if !chat_banned_player_names.is_empty() {
       start_messages.push(format!("Some players in this game have been muted: {}", chat_banned_player_names.join(", ")));
     }
